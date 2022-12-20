7d7bd3d52ac9ee399d5585b26dc5331192b88055
Sumaiya-topu <topusumaiya@gmail.com>
2022-12-20 16:57:22 +0600

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# realtor-cavern-server
---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+/*
+DB_USER=realtorUser
+DB_PASSWORD=0r5oNzrrusUWNOjD
+*/
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send({ status: "success" });
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qkzu9ty.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const dbClient = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const projectCollection = dbClient
+      .db("realtorCavern")
+      .collection("projects");
+
+    /*  GET (/projects)
+        GET (/projects/:projectId)
+        PUT (/project/:projectId)
+    */
+
+    app.get("/projects", async (req, res) => {
+      const query = {};
+      const cursor = projectCollection.find(query);
+      const projects = await cursor.toArray();
+      res.send(projects);
+    });
+  } finally {
+  }
+}
+run().catch((error) => console.error(error));
+
+app.listen(port, () => {
+  console.log(` server running on ${port}`);
+});
